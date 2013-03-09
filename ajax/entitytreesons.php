c2e9812f563a9d671f0492912d71678c9f99ce9a
yllen <yllen@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2013-03-09 12:03:22 +0000

    [0.85] fix r20162 (moyo to check please)
    git-svn-id: https://forge.indepnet.net/svn/glpi/branches/0.85-work@20368 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- ajax/entitytreesons.php
+++ ajax/entitytreesons.php
@@ -3,7 +3,7 @@
  * @version $Id$
  -------------------------------------------------------------------------
  GLPI - Gestionnaire Libre de Parc Informatique
- Copyright (C) 2003-2012 by the INDEPNET Development Team.
+ Copyright (C) 2003-2013 by the INDEPNET Development Team.
 
  http://indepnet.net/   http://glpi-project.org
  -------------------------------------------------------------------------
@@ -33,8 +33,7 @@
 
 $AJAX_INCLUDE = 1;
 
-define('GLPI_ROOT','..');
-include (GLPI_ROOT."/inc/includes.php");
+include (GLPI_ROOT."../inc/includes.php");
 
 header("Content-Type: text/html; charset=UTF-8");
 Html::header_nocache();
