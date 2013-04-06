493ce8dadc1f762ed75c5543e5241fe51863e393
yllen <yllen@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2013-04-06 16:12:42 +0000

    [0.85] fix login - see #4227
    git-svn-id: https://forge.indepnet.net/svn/glpi/branches/0.85-work@20668 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/session.class.php
+++ inc/session.class.php
@@ -70,7 +70,7 @@ class Session {
    **/
    static function init(Auth $auth) {
       global $CFG_GLPI;
-
+      toolbox::logdebug("auth", $auth);
       if ($auth->auth_succeded) {
          // Restart GLPi session : complete destroy to prevent lost datas
          $tosave = array('glpi_plugins', 'glpicookietest', 'phpCAS', 'glpicsrftokens');
@@ -94,8 +94,10 @@ class Session {
 
             if (!$auth->user->fields['is_deleted']
                 && ($auth->user->fields['is_active']
-                    || ($auth->user->fields['begin_date'] < $_SESSION["glpi_currenttime"])
-                    || ($auth->user->fields['is_active'] > $_SESSION["glpi_currenttime"]))) {
+                    && (($auth->user->fields['begin_date'] < $_SESSION["glpi_currenttime"])
+                        || ($auth->user->fields['begin_date'] == NULL))
+                    && (($auth->user->fields['end_date'] > $_SESSION["glpi_currenttime"])
+                        || ($auth->user->fields['end_date'] == NULL)))) {
                $_SESSION["glpiID"]              = $auth->user->fields['id'];
                $_SESSION["glpiname"]            = $auth->user->fields['name'];
                $_SESSION["glpirealname"]        = $auth->user->fields['realname'];
