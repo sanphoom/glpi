82187f7cfd5bc22d35fcffae5b4535fc46e4d833
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2013-05-04 08:35:16 +0000

    [0.85] fix upgrade
    git-svn-id: https://forge.indepnet.net/svn/glpi/branches/0.85-work@20893 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- install/update_084_085.php
+++ install/update_084_085.php
@@ -106,7 +106,7 @@ function update084to085() {
          foreach ($configs as $name => $value) {
             $query = "INSERT INTO `glpi_configs`
                              (`context`, `name`, `value`)
-                      VALUES ('core', '$name', '$value');";
+                      VALUES ('core', '$name', '".addslashes($value)."');";
             $DB->query($query);
          }
       }
