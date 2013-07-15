8d5aa2cffd5ac75c98dfbbc487da236218f9188a
tsmr <tsmr@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2013-07-15 11:45:28 +0000

    [0.85] getText correction
    git-svn-id: https://forge.indepnet.net/svn/glpi/branches/0.85-work@21315 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/ticket.class.php
+++ inc/ticket.class.php
@@ -2276,7 +2276,7 @@ class Ticket extends CommonITILObject {
 
       $tab[27]['table']             = 'glpi_ticketfollowups';
       $tab[27]['field']             = 'count';
-      $tab[27]['name']              = "Number of followups";
+      $tab[27]['name']              = __('Number of followups');
       $tab[27]['forcegroupby']      = true;
       $tab[27]['usehaving']         = true;
       $tab[27]['datatype']          = 'number';
