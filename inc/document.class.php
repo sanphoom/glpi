a4094cdcb5ddf68f5eee53061952579941d7173b
moyo <moyo@35612d4c-9d10-0410-837c-bf9dd1be60d7>
2013-08-09 08:17:40 +0000

    [0.85] minor fix
    git-svn-id: https://forge.indepnet.net/svn/glpi/branches/0.85-work@21477 35612d4c-9d10-0410-837c-bf9dd1be60d7

--- inc/document.class.php
+++ inc/document.class.php
@@ -406,12 +406,15 @@ class Document extends CommonDBTM {
          $fileout = $this->fields['filename'];
       }
 
+      $initfileout = $fileout;
+      
       if (Toolbox::strlen($fileout) > $len) {
          $fileout = Toolbox::substr($fileout,0,$len)."&hellip;";
       }
 
       $out = "<a href='".$CFG_GLPI["root_doc"]."/front/document.send.php?docid=".
-               $this->fields['id'].$params."' target='_blank'>";
+               $this->fields['id'].$params."' alt=\"".$initfileout."\"
+               title=\"".$initfileout."\"target='_blank'>";
 
       $splitter = explode("/",$this->fields['filepath']);
 
@@ -425,7 +428,7 @@ class Document extends CommonDBTM {
             if ($DB->numrows($result) > 0) {
                $icon = $DB->result($result,0,'icon');
                $out .= "&nbsp;<img class='middle' style='margin-left:3px; margin-right:6px;' alt=\"".
-                               $fileout."\" title=\"".$fileout."\" src='".
+                               $initfileout."\" title=\"".$initfileout."\" src='".
                                $CFG_GLPI["typedoc_icon_dir"]."/$icon'>";
             }
          }
